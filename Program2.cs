b2280dca1fbcc1324bac470b473b74723c1b1724
LordNorbo <schweibertnorbert@gmail.com>
2025-04-06 14:28:42 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masodik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            
+            
+
+            string nev; //egy szöveg típusú változó deklarációja
+            Console.Write("Hogy hívnak? ");
+            nev = Console.ReadLine(); //név változónak értékül adjuk azt, amit a felhasználó begépel
+
+            Console.WriteLine("Jó napot kívánok kedves" + nev + "!");
+
+            Console.ReadKey();
+
+            int kor; // ez egy egész típusú változó
+            Console.Write("Hány éves vagy? ");
+            kor = Convert.ToInt32(Console.ReadLine()); // a beolvasott sort egész számmá konvertáljuk
+
+            Console.WriteLine(nev + " " + kor + "éves");
+
+            Console.ReadKey();
+        }
+    }
+}
