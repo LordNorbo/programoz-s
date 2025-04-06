58f4c679e17310f31421532f865a82aecdae91d8
LordNorbo <schweibertnorbert@gmail.com>
2025-04-06 14:28:15 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helló világ!");
+            Console.WriteLine("Suiiiii");
+            Console.ReadKey();
+            Console.Write("egy sorban vagyunk");
+            Console.Write(" még mindig essa");
+            Console.ReadKey();
+        }
+    }
+}
