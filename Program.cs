a4a2e3b0c97f663df561a22f7f154076e2b11925
LordNorbo <schweibertnorbert@gmail.com>
2025-04-06 14:23:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,222 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tombok_feladat
+namespace harmadik
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //1.
-            int[] vasgyurok = new int[100];
-            int osszeg = 0;
-            Random rnd = new Random();
+            /*
+             * 1. feladat: Kérd be egy téglalap oldalainak hosszát.
+             * Írd ki a területét és a kerületét
+             * */
+            //int a;
+            //int b;
+            //Console.Write("Kérem az 'a' oldal hosszát egész számként ");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Kérem a 'b' oldal hosszát egész számként ");
+            //b = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < vasgyurok.Length; i++)
-            {
-                vasgyurok[i] = rnd.Next(25, 65);
-                
-            }
+            //Console.WriteLine("Kerület=" + (2 * a + 2 * b) + "cm");
+            //Console.WriteLine("Terület=" + (a * b) + "cm2");
 
-            //a
+            //Console.ReadKey();
 
-            for (int i = 0; i < vasgyurok.Length; i++)
-            {
-                osszeg = osszeg + vasgyurok[i];
-            }
+            /* 2. feladat: Írj be két egész számot és írd ki az
+             * -összegüket
+             * -különbségüket(elsőből a második)
+             * -hányadosukat(elsőben a második)
+             * -a maradékot(elsőben a második)
+             * -a szorzatot
+             * */
 
-            Console.WriteLine("Átlag:"+osszeg/vasgyurok.Length+"cm");
+            //int c;
+            //int d;
+            //Console.Write("Kérem az 'c' értékét egész számként ");
+            //c = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Kérem a 'd' értékét egész számként ");
+            //d = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadKey();
+            //Console.WriteLine("Kérem a két szám összegét");
+            //Console.WriteLine("összeg=" + (c + d));
 
-            //b
+            //Console.WriteLine("Kérem a két szám különbségét");
+            //Console.WriteLine("különbség=" + (c-d));
 
-            int szamlalo = 0;
-            for (int i = 0; i < vasgyurok.Length; i++)
-            {
-                if (vasgyurok[i]>50)
-                {
-                    szamlalo++;
-                }
-            }
-            Console.WriteLine("50cm-nél nagyobb karú rabok: "+szamlalo+"db");
+            //Console.WriteLine("Kérem a két szám hányadosát");
+            //Console.WriteLine("hányados=" + (c/d));
 
-            Console.ReadKey();
+            //Console.WriteLine("Kérem a két szám maradékát");
+            //Console.WriteLine("maradék=" + (c%d));
 
-            //c
+            //Console.WriteLine("Kérem a két szám szorzatát");
+            //Console.WriteLine("szorzat=" + (c*d));
 
-            int legnagyobb = vasgyurok[0];
+            //Console.ReadKey();
 
-            for (int i = 1; i < vasgyurok.Length; i++)
-            {
-                if (vasgyurok[i]>legnagyobb)
-                {
-                    legnagyobb = vasgyurok[i];
-                }
-            }
-            Console.WriteLine("A legnagyobb kar: "+legnagyobb+"cm");
+            /* 3. feladat: Kérd be a felhasználó vezetéknevét és keresztnevét!
+             * -írd ki a teljes nevét egy sorba
+             * -köszönj neki angolul és magyarul is két külön utasításban */
 
-            Console.ReadKey();
+            //string keresztnev;
+            //string vezeteknev;
+            //Console.Write("Mi a keresztneved?" );
+            //keresztnev = Console.ReadLine();
 
-            //d
+            //Console.Write("Mi a vezetékneved?" );
+            //vezeteknev = Console.ReadLine();
+            //Console.WriteLine("teljes név:" + (keresztnev +  vezeteknev));
 
-            int j = 0;
+            //Console.WriteLine("Üdv" +  (keresztnev +  vezeteknev));
+            //Console.WriteLine("Welcome" +  (vezeteknev +  keresztnev));
+            //Console.ReadKey();
 
-            while (j<vasgyurok.Length && vasgyurok[j]<=60)
-            {
-                j++;
-            }
-            if (j<vasgyurok.Length)
-            {
-                Console.WriteLine("Van 60cm-nél nagyobb karú rab");
-            }
-            else
-            {
-                Console.WriteLine("Nincs 60cm-nél nagyobb karú rab");
-            }
+            //Házi feladat: Készítsd el a gondoltam egy számra játékot!
 
-            Console.ReadKey();
-            //2.
-
-            int[] jegyek = new int[30];
-            for (int i = 0; i < jegyek.Length; i++)
-            {
-                jegyek[i] = rnd.Next(1, 6);
-            }
-
-            //a
-            j = 0;
-
-            while (j<jegyek.Length && jegyek[j]!=1)
-            {
-                j++;
-            }
-            if (j<jegyek.Length)
-            {
-                Console.WriteLine("Van bukott diák a "+(j+1)+". helyen");
-            }
-            else
-            {
-                Console.WriteLine("Nincs bukott diák");
-            }
-            Console.ReadKey();
-
-            //b
-            szamlalo = 0;
-            for (int i = 0; i < jegyek.Length; i++)
-            {
-                if (jegyek[i]==5)
-                {
-                    szamlalo++;
-                }
-            }
-            Console.WriteLine("Ennyi jeles tanuló van: " + szamlalo);
-
-            Console.ReadKey();
-
-            //c
-            osszeg = 0;
-            for (int i = 0; i < jegyek.Length; i++)
-            {
-                osszeg = osszeg + jegyek[i];
-            }
-            Console.WriteLine("A csoport átlaga:"+ osszeg/jegyek.Length);
-
-            Console.ReadKey();
-
-            //3. a
-
-            string[] nevek = new string[2];
-            int[] fizika = new int[2];
+            double a;
+            double b;
+           
             
-            for (int i = 0; i < nevek.Length; i++)
-            {
-                Console.WriteLine("Adja meg a tanuló nevét");
-                nevek[i] = Console.ReadLine();
-                //fizika[i] = rnd.Next(1, 6);
-                Console.WriteLine("Adja meg a tanuló jegyét");
-                fizika[i] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Gondolj egy számra");
 
-                Console.WriteLine(nevek[i]+"fizika jegye: " + fizika[i]);
-            }
+            Console.WriteLine("Adj hozzzá 4-et");
 
-            Console.ReadKey();
-            //b
-            int legkisebbhelye = 0;
-
-            for (int i = 0; i < fizika.Length; i++)
-            {
-                if (fizika[i]<fizika[legkisebbhelye])
-                {
-                    legkisebbhelye = 1;
-                }
-            }
-            Console.WriteLine(nevek[legkisebbhelye]+ "a leggyengébb fizikából");
-
-            Console.ReadKey();
-
-            //c
-            j = 0;
-            while (j<fizika.Length&&fizika[j]!=3)
-            {
-                j++;
-            }
-            if (j<fizika.Length)
-            {
-                Console.WriteLine("Van 3mas, mégpedig: "+ nevek[j]);
-            }
-
-            Console.ReadKey();
-
-            //4.
-            string[] osztaly = new string[2];
-            int[] proba = new int[2];
-            for (int i = 0; i < osztaly.Length; i++)
-            {
-                Console.WriteLine("Adja meg a tanuló nevét");
-                osztaly[i] = Console.ReadLine();
-                Console.WriteLine("Adja meg a tanuló probálkozásainak számát");
-                proba[i] = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine(osztaly[i] + "tanuló probálkozásainak száma: "+ proba[i]);
-            }
-            //a
-            j= 0;
-
-            while (j<osztaly.Length && proba[j]!=10)
-            {
-                j++;
-            }
-            if (j<osztaly.Length)
-            {
-                Console.WriteLine("Van olyan aki 10nél többször próbálkozott");
-            }
-            else
-            {
-                Console.WriteLine("Nincs olyan aki 10nél többször próbálkozott");
-            }
-
-            //b
-            Console.ReadKey();
-
-            j = 0;
-
-            while (j < osztaly.Length && proba[j] != 0)
-            {
-                j++;
-            }
-            if (j < osztaly.Length)
-            {
-                Console.WriteLine("Van olyan aki 0szor próbálkozott");
-            }
-            else
-            {
-                Console.WriteLine("Nincs olyan aki 0szor próbálkozott");
-            }
-
-            Console.ReadKey();
+            Console.WriteLine("Vonj ki belőle 6-ot");
+           
             
-            //c
+
+            Console.WriteLine("Mennyi lett a kapott érték?");
+            
+            Console.Write("A kapott szám:" + (b));
+           
+            Console.WriteLine("A gondolt szám:" + (a=b+6-4));
+           
+
+            Console.ReadKey();
 
 
 
